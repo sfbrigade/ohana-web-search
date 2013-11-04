a5869a1b86d4ad9f94ee1cef24bb0f4294ba35d7
Moncef Belyamani <giter@done>
2013-11-03 17:25:27 -0800

    turn auth back on

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,9 +1,9 @@
 HumanServicesFinder::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
-  # config.middleware.use '::Rack::Auth::Basic' do |u, p|
-  #   [u, p] == [ENV["STAGING_USER"], ENV["STAGING_PASSWORD"]]
-  # end
+  config.middleware.use '::Rack::Auth::Basic' do |u, p|
+    [u, p] == [ENV["STAGING_USER"], ENV["STAGING_PASSWORD"]]
+  end
 
   # Code is not reloaded between requests
   config.cache_classes = true
